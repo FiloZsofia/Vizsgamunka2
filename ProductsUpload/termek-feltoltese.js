d25febb500d327d7310963ea4a9197b7a2060240
FiloZsofia <68869637+FiloZsofia@users.noreply.github.com>
2024-04-16 21:04:23 +0200

    Termék feltöltésénél grid javítás, formázás, js a fájlkiválasztáshoz

--- ProductsUpload/termek-feltoltese.js
+++ ProductsUpload/termek-feltoltese.js
@@ -114,5 +114,21 @@ fetch("http://localhost:8080/product/add", {
 let hozzaad = document.getElementById("hozzaadasGomb");
 hozzaad.onclick = feltoltes;
 
+
+// Fájlkiválasztás a formon belülre kattintáskor:
+document.getElementById('upload-form').addEventListener('click', function() {
+  // Amikor a formon belül bármely helyen kattintunk, megnyitjuk a fájlkiválasztó ablakot
+  document.getElementById('file-input').click();
+});
+
+// Fájl kiválasztása után történő eseménykezelő
+document.getElementById('file-input').addEventListener('change', function() {
+  // Az 'input' esemény kiváltása után meg tudjuk állapítani, hogy a felhasználó választott-e fájlt
+  if (this.files && this.files[0]) {
+      // Ha a felhasználó választott egy fájlt, akkor megtehetjük a szükséges műveleteket, pl. fájl feltöltése
+      console.log('Kiválasztott fájl:', this.files[0]);
+  }
+});
+
 $("#navi").load("../Navigation/navigation.html")
 $("#footer").load("../Footer/footer.html")
