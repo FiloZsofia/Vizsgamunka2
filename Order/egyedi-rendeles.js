94919a86cdb9538d232cbdfec77efae8b3330c6a
FiloZsofia <68869637+FiloZsofia@users.noreply.github.com>
2024-04-18 21:43:22 +0200

    Egyedi rendelés javítása, főoldalon a technikák rész újragondolása

--- Order/egyedi-rendeles.js
+++ Order/egyedi-rendeles.js
@@ -23,5 +23,20 @@ async function materials() {
 
 materials();
 
+// Fájlkiválasztás a formon belülre kattintáskor:
+document.getElementById('upload-form').addEventListener('click', function() {
+  // Amikor a formon belül bármely helyen kattintunk, megnyitjuk a fájlkiválasztó ablakot
+  document.getElementById('file-input').click();
+});
+
+// Fájl kiválasztása után történő eseménykezelő
+document.getElementById('file-input').addEventListener('change', function() {
+  // Az 'input' esemény kiváltása után meg tudjuk állapítani, hogy a felhasználó választott-e fájlt
+  if (this.files && this.files[0]) {
+      // Ha a felhasználó választott egy fájlt, akkor megtehetjük a szükséges műveleteket, pl. fájl feltöltése
+      console.log('Kiválasztott fájl:', this.files[0]);
+  }
+});
+
 
 
