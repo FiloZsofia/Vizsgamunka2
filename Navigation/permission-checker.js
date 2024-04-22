f9e9476dd010912704ad2c0977b8ec4776161f64
Tivadar <govamoro@gmail.com>
2024-04-22 20:21:36 +0200

    autentikáció majdnem működik...

--- Navigation/permission-checker.js
+++ Navigation/permission-checker.js
@@ -1,38 +1,32 @@
-import { parseJwt } from "./jwt.js";
-
-// ennek a szarnak minden oldalbetolteskor lekell futnia hoogy megnezzuk hogy mefelelo joggal
-// es valid jwt-vel akarod elerni az oldalt
-function redirectToLogin(pageNameTrimmed) {
+export function redirectToLogin() {
+  console.log(localStorage.getItem("token"));
   const url = "http://127.0.0.1:8080/auth/validate";
 
   fetch(url, {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
-      Authorization: "Bearer " + localStorage.getItem("token"),
+      Authorization: localStorage.getItem("token"),
     },
-  })
+  })  
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
-      let jwtObject = parseJwt(localStorage.getItem("token"));
-
-      // a data a response amit a backended kuld. ha az false akkor eliranyit
-      if (data == false || jwtObject.payload.role != pageNameTrimmed) {
-        window.location.href = "/";
+      if (data == false) {
+        alert("Bejelentkezés szükséges");
+        window.location.href = "/Main/index.html";
+      } else {
+        console.log("Be vagy jelentkezve");
       }
     })
     .catch((error) => {
-      window.location.href = "/";
+      console.log(error);
+      window.location.href = "/Main/index.html";
     });
 }
 
-// ez egy szar megoldas. itt azt csinalom hogy oldalbetoltesekor megnezi hogy milyen url hivta meg pl:
-// http://localhost:3000/user-dashboard.html
-// ebbol kiszedem a user-dashboard.html szoveget
-// majd leszedem belole az elso kotojel elottig mindent tehat marad user
-// ami megeggyezik a jogoddal tehat elered :D
-window.addEventListener("DOMContentLoaded", function () {
-  let param = document.location.pathname.substring(1);
-  redirectToLogin(param.split("-")[0]);
+document.addEventListener("DOMContentLoaded", function() {
+  redirectToLogin();
 });
+
+
+
