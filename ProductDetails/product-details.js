83de0b9560c978ef57e4b39ef34f621074ee576f
Tivadar <govamoro@gmail.com>
2024-04-23 17:51:22 +0200

    peroductDetails működik

--- ProductDetails/product-details.js
+++ ProductDetails/product-details.js
@@ -13,7 +13,7 @@ $("#footer").load("../Footer/footer.html");
             let termekAdatok = JSON.parse(xhr.responseText);
 
             // A feldolgozott adatok használata a termek függvényben
-            termekAdatok.forEach(function(termekAdat) {
+            termekAdatok.forEach(function(data) {
                 let box = document.createElement("div");
                 box.className = "box";
         
@@ -21,36 +21,36 @@ $("#footer").load("../Footer/footer.html");
                 imageDiv.className = "image-div";
         
                 let image = document.createElement("img");
-                image.src = termekAdat.imgUrl;
+                image.src = data.imgUrl;
         
                 let content = document.createElement("div");
                 content.className = "content";
               
                 let festmenyNevek = document.createElement("h3");
-                festmenyNevek.innerText = termekAdat.title;
+                festmenyNevek.innerText = data.title;
         
                 let artist = document.createElement("p");
-                artist.innerText = termekAdat.artist;
+                artist.innerText = data.artist;
         
                 let createdYear = document.createElement("p");
-                createdYear.innerText = termekAdat.createdYear;
+                createdYear.innerText = data.createdYear;
         
                 let material = document.createElement("p");
-                material.innerText = termekAdat.material;
+                material.innerText = data.material;
         
                 let style = document.createElement("p");
-                style.innerText = termekAdat.style;
+                style.innerText = data.style;
         
                 let description = document.createElement("p");
-                description.innerText = termekAdat.description;
+                description.innerText = data.description;
         
                 let price = document.createElement("p");
                 price.className = "price";
-                price.innerText = `${termekAdat.price.toLocaleString()} Ft`;
+                price.innerText = `${data.price.toLocaleString()} Ft`;
         
                 let size = document.createElement("p");
                 size.className = "size";
-                size.innerText = `A termék mérete: ${termekAdat.xcm} x ${termekAdat.ycm} cm`;
+                size.innerText = `A termék mérete: ${data.xcm} x ${data.ycm} cm`;
         
                 let kosar = document.createElement("button");
                 kosar.className = "kosar";
@@ -76,11 +76,77 @@ $("#footer").load("../Footer/footer.html");
 }
 
 termek();*/
+let section = document.getElementById("termek");
+const urlParams = new URLSearchParams(window.location.search);
+const termekAzonosito = urlParams.get('id');
+const url = "http://localhost:8080/product/get/" + termekAzonosito ;
 
-async function productById(id) {
-    const url = "http://localhost:8080/product/get/" + id ;
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log(data);
-    window.location.href = "../ProductDetails/product-details.html";
-  }
+fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+
+                //IDE KELL KERÜLJÖN AZ ADATFELTÖLTÉS
+                //let kep = document.getElementById("kep");
+                //kep.src = data.imgUrl;
+                let box = document.createElement("div");
+                box.className = "box";
+        
+                let imageDiv = document.createElement("div");
+                imageDiv.className = "image-div";
+        
+                let image = document.createElement("img");
+                image.src = data.imgUrl;
+        
+                let content = document.createElement("div");
+                content.className = "content";
+              
+                let festmenyNevek = document.createElement("h3");
+                festmenyNevek.innerText = data.title;
+        
+                let artist = document.createElement("p");
+                artist.innerText = data.artist;
+        
+                let createdYear = document.createElement("p");
+                createdYear.innerText = data.createdYear;
+        
+                let material = document.createElement("p");
+                material.innerText = data.material;
+        
+                let style = document.createElement("p");
+                style.innerText = data.style;
+        
+                let description = document.createElement("p");
+                description.innerText = data.description;
+        
+                let price = document.createElement("p");
+                price.className = "price";
+                price.innerText = `${data.price.toLocaleString()} Ft`;
+        
+                let size = document.createElement("p");
+                size.className = "size";
+                size.innerText = `A termék mérete: ${data.xcm} x ${data.ycm} cm`;
+        
+                let kosar = document.createElement("button");
+                kosar.className = "kosar";
+                kosar.innerText = "Kosárba";
+        
+                imageDiv.appendChild(image);
+                box.appendChild(content);
+                box.appendChild(imageDiv);
+                content.appendChild(festmenyNevek);
+                content.appendChild(artist);
+                content.appendChild(createdYear);
+                content.appendChild(material);
+                content.appendChild(style);
+                content.appendChild(description);
+                content.appendChild(price);
+                content.appendChild(size);
+                content.appendChild(kosar);
+                section.appendChild(box);
+            })
+            .catch(error => {
+                // Ha hiba történik, kiírjuk a konzolra
+                console.error('Hiba történt a termék adatok lekérdezésekor:', error);
+            });
+
