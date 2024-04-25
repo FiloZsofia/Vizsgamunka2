0af0b944be4c649f554d1e21bccf99124be937b1
Tivadar <govamoro@gmail.com>
2024-04-25 18:07:20 +0200

    product detail teljesen működik

--- ProductDetails/product-details.js
+++ ProductDetails/product-details.js
@@ -11,12 +11,28 @@ fetch(url)
             .then(response => response.json())
             .then(data => {
                 console.log(data);
+                
+                function getMaterial(jsonObj) {
+                    if (!jsonObj.material) {
+                        return "Nincs elegendő adat a JSON objektumban.";
+                    }
+                    const materialNames = jsonObj.material.map(mat => mat.name).join(', ');
+                    return materialNames;
+                }
+
+                function getStyle(jsonObj) {
+                    if (!jsonObj.style) {
+                        return "Nincs elegendő adat a JSON objektumban.";
+                    }
+                    const styleNames = jsonObj.style.map(st => st.name).join(', ');
+                    return styleNames;
+                }
 
                 document.getElementById("title").textContent = data.title;
                 document.getElementById("artist").innerHTML = "<b>Művész: </b>" + data.artist;
                 document.getElementById("created-year").innerHTML = "<b>Készítés éve: </b>" + data.createdYear;
-                document.getElementById("material").innerHTML = "<b>Technika: </b>" + data.material;
-                document.getElementById("style").innerHTML = "<b>Téma: </b>" + data.style;
+                document.getElementById("material").innerHTML = "<b>Technika: </b>" + getMaterial(data);
+                document.getElementById("style").innerHTML = "<b>Téma: </b>" + getStyle(data);
                 document.getElementById("size").innerHTML = "<b>Méret: </b>" + data.xcm + " x " + data.ycm + " cm";
                 document.getElementById("description").innerHTML = "<b>Leírás: </b>" + data.description;
                 document.getElementById("price").innerHTML = data.price.toLocaleString() + " Ft";
